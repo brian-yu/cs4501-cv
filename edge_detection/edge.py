52e401963d5c8b42e6a1e945f001892be296f40a
aaron-gu <ag5ym@virginia.edu>
2019-12-27 13:54:01 -0500

    edge detection stuff

--- edge_detection/edge.py
+++ edge_detection/edge.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import argparse
+# import imageio
+import math
 import numpy as np
 
 parser = argparse.ArgumentParser(
@@ -47,67 +49,25 @@ net = cv.dnn.readNet(args.prototxt, args.caffemodel)
 ## Create a display window
 kWinName = 'Holistically-Nested_Edge_Detection'
 cv.namedWindow(kWinName, cv.WINDOW_AUTOSIZE)
-
 cap = cv.VideoCapture(args.input if args.input else 0)
-
-if args.write_video:
-    # Define the codec and create VideoWriter object
-    w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-    print(w,h)
-    # w, h = args.width,args.height
-    fourcc = cv.VideoWriter_fourcc(*'MP4V')
-    writer = cv.VideoWriter(args.savefile, fourcc, 25, (w, h))
-# while cv.waitKey(1) < 0:
 hasFrame, frame = cap.read()
 width = frame.shape[1]
 height = frame.shape[0]
-# if not hasFrame:
-#     cv.waitKey()
-#     break
 inp = cv.dnn.blobFromImage(frame, scalefactor=1.0, size=(args.width, args.height),
                            mean=(104.00698793, 116.66876762, 122.67891434),
                            swapRB=False, crop=False)
 net.setInput(inp)
+print('computing edges')
 out = net.forward()
 out = out[0, 0]
 out = cv.resize(out, (frame.shape[1], frame.shape[0]))
 out = 255 * out
 out = out.astype(np.uint8)
 bgrout=cv.cvtColor(out,cv.COLOR_GRAY2BGR)
-con=np.concatenate((frame,bgrout),axis=1)
-if args.write_video:
-    writer.write(np.uint8(con))
-# cv.imshow(kWinName,con)
-
-# # Determine contour of all blobs found
-# _, contours0, hierarchy = cv.findContours( out.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-# contours = [cv.approxPolyDP(cnt, 3, True) for cnt in contours0]
-
-# # Draw all contours
-# vis = np.zeros((h, w, 3), np.uint8)
-# cv.drawContours( vis, contours, -1, (128,255,255), 3, cv.LINE_AA)
-
-# cv.imwrite('contours.png', vis)
-
-lines = cv.HoughLines(out, 1, np.pi/180, 100) # img, rho sensitivity, theta sensitivity, threshold
-print(lines[0])
-for rho,theta in lines[0]:
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + height*(-b))
-    y1 = int(y0 + width*(a))
-    x2 = int(x0 - height*(-b))
-    y2 = int(y0 - width*(a))
-
-    cv.line(out,(x1,y1),(x2,y2),(0,0,255),2)
-
-cv.imwrite('houghlines.jpg',out)
-
 cv.imwrite('test.png', bgrout)
-cv.imwrite('test_concat.png', con)
+
+# con=np.concatenate((frame,bgrout),axis=1)
+# cv.imwrite('test_concat.png', con)
 
 # cv.waitKey(0)
 cv.destroyAllWindows()
